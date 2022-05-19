3f7e5c01f1d83671971625282c4b2e125c36c2a5
JHessler <jhessler11@gmail.com>
2022-05-18 21:02:59 -0400

    contract form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
+import ContactForm from './componenets/Contact';
 import Header from './componenets/Header';
 import Nav from './componenets/Nav';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false)
   return (
     <div>
       <Header></Header>
       <Nav></Nav>
+      <main>
+        <ContactForm></ContactForm>
+      </main>
     </div>
   );
 }
