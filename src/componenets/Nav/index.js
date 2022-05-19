bbf9b43e8d31936b0019584a2957f63c0e54dc6c
JHessler <jhessler11@gmail.com>
2022-05-18 20:01:40 -0400

    nav

---
+++ src/componenets/Nav/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+function Nav(props) {
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a>About ME
+
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Portfolio
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Contact
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Resume
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+
+}
+
+export default Nav;
