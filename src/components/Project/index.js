5d2e20917efb32ac10c322a82b4d86a9b42a77c1
JHessler <jhessler11@gmail.com>
2022-05-23 10:20:44 -0400

    updating portfolio

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,16 +3,21 @@
 function Portfolio() {
 
     return (
-        <section>
-            <div>
-                Github
+        <section className="columns  is-multiline is-mobile is-centered">
+            <div className="column is-one-third">
+                    <img src="/images/weather-app.png"  alt="weather application" title="Weather Application"/>
+                <div className="portGit">
+                    <a href='https://github.com/JHESSLER11/Weather-Application'>
+                    <img src='/images/GitHub-Mark-32px.png' alt='Github Logo'></img>
+                    </a>
+                    <div>Web API's</div>
+                </div>
             </div>
-            <div>
-                Linkedin
-            </div>
-            <div>
-                Stack Overflow
-            </div>
+            <div className="column is-one-third">2</div>
+            <div className="column is-one-third">3</div>
+            <div className="column is-one-third">4</div>
+            <div className="column is-one-third">5</div>
+            <div className="column is-one-third">6</div>
         </section>
     )
 
