e949fa8d1e9227308afe54cd194839a2af422b14
JHessler <jhessler11@gmail.com>
2022-05-23 08:37:32 -0400

    download resume working

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,29 @@
 
-function Resume() {
 
+function Resume() {
+    
+    // const proficiencies = ['Javascript', 'React', 'CSS', 'Html5', 'MySQL', 'MongoDB', 'Express', 'PWA', 'VSCode', 'Rest API', 'Github', 'AcrGIS Software', 'AutoCAD Civil 3D', 'Googling']
+    
+    // let list = document.getElementById('pList')
+    // console.log(list)
+    
+    // proficiencies.forEach((item) => {
+    //     console.log('stuff', item)
+    //     let li = document.createElement("li");
+    //     li.innerText = item;
+    //     console.log(item)
+    //     list.appendChild(li);
+    //     console.log("bang", li)
+    // })
     return (
-       <div>
-           words
-       </div>
+       
+       <section>
+           <h2>Proficiencies</h2>
+            <ul id="pList"></ul>
+            <div>
+                <a href="/pdf/jhessler.pdf" download="jhessler">Download Resume!</a>
+            </div>
+       </section>
     )
 
 }
