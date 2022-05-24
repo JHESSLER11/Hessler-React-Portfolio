3bdfeadcb446f8b588b8692e5cf2107ff6169ee5
JHessler <jhessler11@gmail.com>
2022-05-24 19:23:59 -0400

    updated images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,17 +8,17 @@ function Footer() {
             <div className="foot content has-text-centered">
                 <div className='icon m-2 image is-48x48'>
                     <a href='https://github.com/JHESSLER11'>
-                    <img src='/images/GitHub-Mark-32px.png' alt='Github Logo'></img>
+                    <img src= {process.env.PUBLIC_URL + '/images/GitHub-Mark-32px.png'} alt='Github Logo'></img>
                     </a>
                 </div>
                 <div className='icon m-2 image is-48x48'>
-                <a href='https://www.linkedin.com/in/hessler11/'>
-                    <img src='/images/linkedin-32.png' alt='linkedin Logo'></img>
+                <a href= 'https://www.linkedin.com/in/hessler11/'>
+                    <img src= {process.env.PUBLIC_URL + '/images/linkedin-32.png'} alt='linkedin Logo'></img>
                     </a>
                 </div>
                 <div className='icon m-2 image is-48x48'>
-                <a href='https://stackoverflow.com/users/18665163/hess'>
-                    <img src='/images/stack-overflow-32.png' alt='Stack Overflow Icon'></img>
+                <a href= 'https://stackoverflow.com/users/18665163/hess'>
+                    <img src= {process.env.PUBLIC_URL + '/images/stack-overflow-32.png'} alt='Stack Overflow Icon'></img>
                     </a>
                     
                 </div>
