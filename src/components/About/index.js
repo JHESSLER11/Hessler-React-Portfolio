48bd1cee0556308d3031c15554febe76d7231932
JHessler <jhessler11@gmail.com>
2022-05-24 19:00:33 -0400

    maybe fix picture

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
                 </h1>
             </div>
             <div className="image is-128x128 m-3">
-                <img className="is-rounded" src='/images/Jordan2021.jpeg' alt="Jordan"/>
+                <img className="is-rounded" src={process.env.PUBLIC_URL + '/images/Jordan2021.jpeg'} alt="Jordan"/>
             </div>
             <div className="is-size-5 p-3">
                 I'm currently an environmental designer working towards a certificate in full stack web development from Chapel Hill. My work now consist of designing streams and wetlands to offset environmental impacts produced by development. I'm passionate about environment and  environmental conservation. I'm looking to continue to protect and preserve the environment using my new skill set with my existing environmental design skill set. Although, I can write frontend applications. I prefer to work on the backend of projects. 
